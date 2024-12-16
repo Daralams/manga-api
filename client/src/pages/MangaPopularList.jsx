96f6017f1bd519b6ab694060a40efa29c6fa18fc
ridho777 <ridho.artsys@gmail.com>
2024-12-16 21:04:09 +0700

    [ADD] Manga-API Page && add libraryreact-router-dom dan react-dom

---
+++ client/src/pages/MangaPopularList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MangaPopularList = () => {
+  return (
+    <div>MangaPopularList</div>
+  )
+}
+
+export default MangaPopularList
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "./HomePage";
+import MangaBookmarkList from "./MangaBookmarkList";
+import MangaList from "./MangaList";
+import MangaPopularList from "./MangaPopularList";
+
+export {HomePage, MangaBookmarkList, MangaList, MangaPopularList}
